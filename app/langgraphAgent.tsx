fdcaaf57ecfa80bde5bf5b51890a483168d6014c
praveenc1902 <mailpraveenreddy.c@gmail.com>
2025-08-14 10:38:29 +0530

    added env

--- app/langgraphAgent.tsx
+++ app/langgraphAgent.tsx
@@ -3,7 +3,7 @@ import { UploadedFile, EvaluationResponse } from "./types";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatOpenAI } from "@langchain/openai";
 import { z } from "zod";
-
+import "dotenv/config";
 // const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 // console.log("Using OpenAI API Key:", OPENAI_API_KEY);
 // console.log("Using OpenAI API Key:", process.env.OPENAI_API_KEY);
